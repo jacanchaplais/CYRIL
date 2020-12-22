14d19fd6b1c6fc991ee466bad65d02e454a43501
jc1765@york.ac.uk <jc1765@york.ac.uk>
2020-12-22 00:06:40 +0000

    directory structure and dependencies established

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Productivity management and tracking, with analytics and visualisations to provide helpful insights into your working habits.',
+    author='Hardcoders',
+    license='MIT',
+)
